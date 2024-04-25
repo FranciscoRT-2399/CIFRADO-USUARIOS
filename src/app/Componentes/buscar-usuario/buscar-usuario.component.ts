cc4eda4b9fc51313290998bec4d0712902efdeef
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-25 09:59:32 -0600

    create and update funciones

--- src/app/Componentes/buscar-usuario/buscar-usuario.component.ts
+++ src/app/Componentes/buscar-usuario/buscar-usuario.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ServidorService } from '../../Servidor/servidor.service';
+import { Router } from '@angular/router';
+import { Registro } from '../../Entidad/Registro';
 
 @Component({
   selector: 'app-buscar-usuario',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './buscar-usuario.component.html',
-  styleUrl: './buscar-usuario.component.css'
+  styleUrl: './buscar-usuario.component.css',
 })
 export class BuscarUsuarioComponent {
+  constructor(private router: Router, private service: ServidorService) {}
 
+  user: Registro = new Registro();
+  userBuscado: Registro = new Registro();
+
+  buscarPorUsuario(usuario: String) {
+    if (usuario.trim() == '') {
+      this.userBuscado = { id: 0, usuario: '', password: '' };
+    } else {
+      this.service.buscarRegistroUsuario(usuario).subscribe((data) => {
+        this.userBuscado = data;
+      });
+    }
+  }
+	
 }
--- src/app/Componentes/listar-cifrado/listar-cifrado.component.ts
+++ src/app/Componentes/listar-cifrado/listar-cifrado.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ServidorService } from '../../Servidor/servidor.service';
+import { Registro } from '../../Entidad/Registro';
 
 @Component({
   selector: 'app-listar-cifrado',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './listar-cifrado.component.html',
   styleUrl: './listar-cifrado.component.css'
 })
-export class ListarCifradoComponent {
+export class ListarCifradoComponent implements OnInit{
+
+	constructor(private router: Router, private service: ServidorService){}
+
+	usuarios !: Registro[];
+	user : Registro = new Registro();
+
+	ngOnInit(): void {
+		this.listar();
+	}
+
+	listar(){
+		this.service.listarRegistrosCifrados().subscribe(data => {
+			this.usuarios = data;
+		})
+	}
 
 }
