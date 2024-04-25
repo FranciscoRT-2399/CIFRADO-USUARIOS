9af6b9ccb5ebe26a82bc5c22051aa6030160b923
FranciscoRT-2399 <flash2_@hotmail.com>
2024-04-25 13:10:12 -0600

    add buscar update

--- src/app/Componentes/buscar-id/buscar-id.component.ts
+++ src/app/Componentes/buscar-id/buscar-id.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ServidorService } from '../../Servidor/servidor.service';
+import { Registro } from '../../Entidad/Registro';
 
 @Component({
   selector: 'app-buscar-id',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './buscar-id.component.html',
-  styleUrl: './buscar-id.component.css'
+  styleUrl: './buscar-id.component.css',
 })
 export class BuscarIdComponent {
+  constructor(private service: ServidorService) {}
 
+  user: Registro = new Registro();
+  userBuscado: Registro = new Registro();
+
+  buscarPorId(id: number) {
+    if (id == null) {
+      this.userBuscado = { id: 0, usuario: '', password: '' };
+    } else {
+      this.service.buscarRegistroId(id).subscribe((data) => {
+        this.userBuscado = data;
+      });
+    }
+  }
 }
